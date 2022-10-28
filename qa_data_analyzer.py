79de9c2a7ea505d3ac142076176763e774ad4b2f
diraculix <lukaswolter98@gmail.com>
2022-10-28 14:58:19 +0200

    updated to seabon plots

--- qa_data_analyzer.py
+++ qa_data_analyzer.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.patches import Ellipse
+import seaborn as sns
 
 try:
     df_destination = r'N:\fs4-HPRT\HPRT-Docs\Lukas\Logfile_Extraction\dataframes'  # TO BE CHANGED
@@ -15,23 +15,35 @@ except:
 def plot_qa_beams(qa_df):
     beam_ids = qa_df['BEAM_ID'].drop_duplicates().to_list()
     n_beams = len(beam_ids)
-    for i in range(int(np.sqrt(n_beams)) - 1, n_beams):
-        if n_beams % i == 0:
-            rows = i
-            cols = int(n_beams / i)
-            break
+    sns.set()
+    sns.scatterplot(data=qa_df, x='X_POSITION(mm)', y='Y_POSITION(mm)', hue='LAYER_ENERGY(MeV)', size='X_WIDTH(mm)')
+    plt.show()
+    plt.clf()
+
+    sns.histplot(data=qa_df[['DELTA_X(mm)','DELTA_Y(mm)']], kde=True)
+    plt.xlabel('$\Delta$ (log - plan) [mm]')
+    plt.legend(title='Median position')
+    plt.show()
+
+    only_one_angle = qa_dataframe.loc[qa_dataframe['GANTRY_ANGLE'] == 0.0]
+    only_one_energy = qa_dataframe.loc[qa_dataframe['LAYER_ENERGY(MeV)'] == 100.]
+    sns.pairplot(data=only_one_angle[['DELTA_X(mm)', 'DELTA_Y(mm)', 'X_WIDTH(mm)', 'Y_WIDTH(mm)', 'LAYER_ENERGY(MeV)', 'GANTRY_ANGLE']], vars=['DELTA_X(mm)', 'DELTA_Y(mm)', 'X_WIDTH(mm)', 'Y_WIDTH(mm)'], hue='LAYER_ENERGY(MeV)', corner=True)
+    plt.suptitle('Gantry Angle 0.0°', fontweight='bold')
+    plt.show()
+    sns.pairplot(data=only_one_energy[['DELTA_X(mm)', 'DELTA_Y(mm)', 'X_WIDTH(mm)', 'Y_WIDTH(mm)', 'LAYER_ENERGY(MeV)', 'GANTRY_ANGLE']], vars=['DELTA_X(mm)', 'DELTA_Y(mm)', 'X_WIDTH(mm)', 'Y_WIDTH(mm)'], hue='GANTRY_ANGLE', corner=True)
+    plt.suptitle('Beam Energy 100.0 MeV', fontweight='bold')
+    plt.show()
+
+    return None
 
     # fig, axs = plt.subplots(4, 3, sharex=True, sharey=True, figsize=(20/4, 20/3), dpi=100)  # initiate matrix-like layer plot
-    fig, axs = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(8, 3), dpi=100)  # initiate matrix-like layer plot
+    fig, axs = plt.subplots(5, 5, sharex=True, sharey=True, figsize=(8, 8), dpi=100)  # initiate matrix-like layer plot
     ax0 = fig.add_subplot(111, frameon=False)
     ax0.set_xticks([]), ax0.set_yticks([])
     fig.subplots_adjust(hspace=0.0, wspace=0.0)
     axs = axs.ravel()
     for beam_no, beam_id in enumerate(beam_ids):
-        if beam_no > 2:
-            break
-
-        beam_df = qa_df.loc[qa_df['BEAM_ID'] == beam_id]
+        beam_df = qa_df.loc[(qa_df['BEAM_ID'] == beam_id) & (np.round(qa_df['LAYER_ENERGY'], 1) == qa_energies[0])]
         axs[beam_no].set_axisbelow(True)
         axs[beam_no].grid(which='major', axis='both')
         axs[beam_no].scatter(beam_df['X_POSITION(mm)'], beam_df['Y_POSITION(mm)'], marker='o', c=beam_df['X_WIDTH(mm)'], s=beam_df['X_WIDTH(mm)'], vmin=7, vmax=19, cmap='inferno')
@@ -47,7 +59,7 @@ def plot_qa_beams(qa_df):
     qa_dataframe.plot('LAYER_ENERGY(MeV)', 'X_WIDTH(mm)', kind='scatter')
     qa_dataframe.plot('LAYER_ENERGY(MeV)', 'Y_WIDTH(mm)', kind='scatter')
     
-    qa_energies = [100., 140., 165., 185., 205., 226.7]
+    
     qa_angles = np.linspace(0., 360., 8, endpoint=False)        
 
     for energy in qa_energies:
@@ -74,8 +86,26 @@ def plot_qa_beams(qa_df):
     return None
         
 
+def post_process(qa_df):
+    qa_energies = [100., 140., 165., 185., 205., 226.7]
+    qa_xy = [0., 30., 60., 120.]
+    qa_xy += [- xy for xy in qa_xy]
+    
+    # print(qa_positions)
+    qa_df['XY'] = [np.array(_).round(-1) for _ in zip(qa_df['X_POSITION(mm)'], qa_df['Y_POSITION(mm)'])]
+    qa_df = qa_df[np.round(qa_df['X_POSITION(mm)'], -1).isin(qa_xy) & np.round(qa_df['Y_POSITION(mm)'], -1).isin(qa_xy)]
+    qa_df['DELTA_X(mm)'] = qa_df['X_POSITION(mm)'] - np.round(qa_df['X_POSITION(mm)'], -1)
+    qa_df['DELTA_Y(mm)'] = qa_df['Y_POSITION(mm)'] - np.round(qa_df['Y_POSITION(mm)'], -1)
+    qa_df['LAYER_ENERGY(MeV)'] = np.round(qa_df['LAYER_ENERGY(MeV)'], 1)
+
+    return qa_df
+    
+    
+
 if __name__ == '__main__':
-    qa_dataframe = pd.read_csv('QA_2017-2022_records_data.csv', dtype={'BEAM_ID':str, 'FRACTION_ID':str})
-    print(qa_dataframe['X_WIDTH(mm)'].min(), qa_dataframe['X_WIDTH(mm)'].max())
-    print(qa_dataframe['Y_WIDTH(mm)'].min(), qa_dataframe['Y_WIDTH(mm)'].max())
+    qa_dataframe = pd.read_csv('QA_2017-2022_records_data.csv', dtype={'BEAM_ID':str, 'FRACTION_ID':int})
+    # print(qa_dataframe['X_WIDTH(mm)'].min(), qa_dataframe['X_WIDTH(mm)'].max())
+    # print(qa_dataframe['Y_WIDTH(mm)'].min(), qa_dataframe['Y_WIDTH(mm)'].max())
+    qa_dataframe = post_process(qa_dataframe)
     plot_qa_beams(qa_dataframe)
+    # get_delta(qa_dataframe)
