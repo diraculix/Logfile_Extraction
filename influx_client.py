d2d7c69958ebb72134d50065c0848e9a4ab42df4
diraculix <lukaswolter98@gmail.com>
2022-11-21 17:36:14 +0100

    Added influx_client.py | Test monitor for establishing InfluxDB connection

---
+++ influx_client.py
@@ -0,0 +1,41 @@
+import psutil
+import time
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# You can generate an API token from the "API Tokens Tab" in the UI
+token = "vHLCAHK5mURBcgOJane0P6GsOyBkaDg1H9cHTGwgEwWlRwS7UuFitjIewPhf_o9AZVwinuzHyDD6c9bL6HN2Ug=="
+org = "OncoRay"
+bucket = "GTR2"
+url = "http://g40invivodossrv:8086"
+
+print(f'Establishing connection to {url}/{org}/{bucket} ..')
+with InfluxDBClient(url=url, token=token, org=org) as client:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    
+    print('> Influx: Connected to database, monitor running ..')
+    while True:
+        host = "PC43doktoranden"
+        try:
+            point = Point("cpu") \
+                .tag("host", host) \
+                .field("used_percent", psutil.cpu_percent(5)) \
+                .time(datetime.utcnow(), WritePrecision.NS)
+
+            write_api.write(bucket, org, point)
+
+            point = Point("mem") \
+                .tag("host", host) \
+                .field("used_percent", psutil.virtual_memory()[2]) \
+                .time(datetime.utcnow(), WritePrecision.NS)
+
+            write_api.write(bucket, org, point)
+            time.sleep(5)
+        
+        except KeyboardInterrupt:
+            print(f'> Influx: Monitor on host {host} stopped, disconnecting client..')
+            break
+    
+    client.close()
