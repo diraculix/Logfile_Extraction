e81197cf325b2b2e78e66466795ad9dd39011a63
diraculix <lukaswolter98@gmail.com>
2022-11-21 17:59:46 +0100

    influx_client.py | specify host

--- influx_client.py
+++ influx_client.py
@@ -1,23 +1,26 @@
-import psutil
-import time
+import os, platform, psutil, time
 from datetime import datetime
-
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# You can generate an API token from the "API Tokens Tab" in the UI
+# access config
 token = "vHLCAHK5mURBcgOJane0P6GsOyBkaDg1H9cHTGwgEwWlRwS7UuFitjIewPhf_o9AZVwinuzHyDD6c9bL6HN2Ug=="
 org = "OncoRay"
 bucket = "GTR2"
 url = "http://g40invivodossrv:8086"
 
-print(f'[{datetime.utcnow()}] Establishing connection to {url}/{org}/{bucket} ..')
-with InfluxDBClient(url=url, token=token, org=org) as client:
-    host = "PC43doktoranden"
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    print(f'[{datetime.utcnow()}] > Influx: Connected to database, monitor running..', end='\r')
+# get host name
+if platform.system() == "Windows":
+    host = platform.uname().node
+else:
+    host = os.uname()[1]  # does not work on windows
 
+# monitor loop
+print(f'[{time.ctime()}] Establishing connection to {url}/{org}/{bucket} as {host}..')
+with InfluxDBClient(url=url, token=token, org=org) as client:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
     while True:
+        print(f'[{time.ctime()}] > Influx: Connected to database, monitor running..', end='\r')
         try:
             point = Point("cpu") \
                 .tag("host", host) \
@@ -32,10 +35,10 @@ with InfluxDBClient(url=url, token=token, org=org) as client:
                 .time(datetime.utcnow(), WritePrecision.NS)
 
             write_api.write(bucket, org, point)
-            time.sleep(5)
+            # time.sleep(5)
         
         except KeyboardInterrupt:
-            print(f'[{datetime.utcnow()}] > Influx: Monitor on host {host} stopped, disconnecting client..')
+            print(f'\n[{time.ctime()}] > Influx: Monitor on host {host} stopped, disconnecting client..')
             break
     
     client.close()
